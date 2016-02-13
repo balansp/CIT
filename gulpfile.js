d421d0e1c154bc11a3c2ad31df7d03f3bd7370f0
balansp <balan.spx@gmail.com>
2016-02-13 12:11:09 +0530

    Vector Logo created

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var	 gulp    	= require('gulp'),
 		 connect		= require('gulp-connect'),
 		 gJade 			= require('gulp-jade'),
 		 sass 			= require('gulp-sass'),
+     svgmin = require('gulp-svgmin'),
 		 gulpif = require('gulp-if'),
 
 
@@ -21,14 +22,17 @@ var	 gulp    	= require('gulp'),
 		 							'javascript':inputDir+'/js/*.js',
 									'css'				:inputDir+'/css/*.css',
 									'jade'			:inputDir+'/*.jade',
-									'sass'			:inputDir+'/sass/*.sass'
+									'sass'			:inputDir+'/sass/*.sass',
+                	'svg'			:inputDir+'/img/*.svg'
+
 								},
 		 output     ={'html'			:outputDir+'/',
 		 							'javascript':outputDir+'/js/',
 									'css'				:outputDir+'/css/',
 									'jade'			:outputDir+'/',
-									'sass'			:outputDir+'/css/'
-								}
+									'sass'			:outputDir+'/css/',
+	                'svg'			 :outputDir+'/img/'
+                };
 
 copyIgnore	=['!'+input.html,'!'+input.javascript,'!'+input.css];
 copyIgnore.push(inputDir+'/**');
@@ -109,7 +113,11 @@ function errorLog(error){
     gulp.watch(input.jade,['jade']);
     gulp.watch(input.sass,['sass']);
   });
-
+  gulp.task('svg-minify', function () {
+      return gulp.src(input.svg)
+          .pipe(svgmin())
+          .pipe(gulp.dest(output.svg));
+  });
 	gulp.task('watch',function(){
     gulp.watch(input.html,['html-minify']);
     gulp.watch(input.javascript,['js-minify']);
